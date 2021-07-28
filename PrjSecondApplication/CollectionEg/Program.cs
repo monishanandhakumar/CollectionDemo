7bbcebea1b77fa7753ad61ef9c497ac6f3721240
monishanandhakumar <mmonishadevi.cse@gmail.com>
2021-07-28 09:29:10 +0530

    change in br

--- PrjSecondApplication/CollectionEg/Program.cs
+++ PrjSecondApplication/CollectionEg/Program.cs
@@ -6,6 +6,7 @@ namespace CollectionEg
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi");
             Console.WriteLine("Hello World!");
         }
     }
